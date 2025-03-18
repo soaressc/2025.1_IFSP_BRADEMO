be5319463fc23e172a5c3b3008a56076358f0a93
Soraya Costa <129467912+soaressc@users.noreply.github.com>
2025-03-17 21:28:52 -0300

    Add files via upload
    Exercício 01 da Aula 02 - Introdução a Flutter

---
+++ Introdução a Flutter/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          shadowColor: Colors.black,
+          elevation: 10,
+          title: Text('Flutter is Fun!', style: TextStyle(color: Colors.white)),
+        ),
+        body: Stack(
+          children: [
+            Container(
+              width: double.infinity,
+              height: double.infinity,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [Colors.white, Colors.black12],
+                ),
+              ),
+            ),
+            Container(
+              width: 120,
+              height: 120,
+              padding: EdgeInsets.all(10.0),
+              margin: EdgeInsets.all(100.0),
+              color: Colors.red,
+              child: Text('Hi Mom 🐣'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
